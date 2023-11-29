0d2b7db8505b950e9c665569fe63b627741a3430
sbmTRT <thureintun@signalbase.co.jp>
2023-11-29 16:38:54 +0900

    TESTING Fastapi 3

--- src/NewApp.vue
+++ src/NewApp.vue
@@ -1,21 +1,21 @@
 <template>
-    <div>
-        <button @click="callFunction">Call Netlify Function</button>
-    </div>
+  <div>
+    <button @click="callFunction">Call Netlify Function</button>
+  </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        async callFunction() {
-            try {
-                const response = await fetch('/.netlify/functions/hello');
-                const data = await response.json();
-                console.log(data.message);
-            } catch (error) {
-            console.error('Error calling function:', error);
-            }
-        },
+  methods: {
+    async callFunction() {
+      try {
+        const response = await fetch('/.netlify/functions/hello');
+        const data = await response.json();
+        alert(data.message); // Show an alert message
+      } catch (error) {
+        console.error('Error calling function:', error);
+      }
     },
+  },
 };
 </script>
