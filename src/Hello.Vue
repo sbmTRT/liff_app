712ad63f9edb36f1d0a939d63027d5a6714cf693
sbmTRT <152240826+sbmTRT@users.noreply.github.com>
2023-11-29 16:27:07 +0900

    Add files via upload

---
+++ src/Hello.Vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button @click="callFunction">Call Netlify Function</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        async callFunction() {
+            try {
+                const response = await fetch('/.netlify/functions/hello');
+                const data = await response.json();
+                console.log(data.message);
+            } catch (error) {
+            console.error('Error calling function:', error);
+            }
+        },
+    },
+};
+</script>
