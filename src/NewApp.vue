5e3c91d659e32489b60aa8d0eac1c520c6335b1b
sbmTRT <thureintun@signalbase.co.jp>
2023-11-29 17:26:15 +0900

    TESTING Fastapi 3

--- src/NewApp.vue
+++ src/NewApp.vue
@@ -9,9 +9,9 @@ export default {
   methods: {
     async callFunction() {
       try {
-        const response =  await fetch('/.netlify/functions/fastapi/get_data');
+        const response = await fetch('/.netlify/functions/fastapi/get_data');
         const data = await response.json();
-        alert(data.message);
+        alert(data.message);s
       } catch (error) {
         console.error('Error calling function:', error);
       }
