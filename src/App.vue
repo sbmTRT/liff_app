94f0c044cb06026991eb6732add5d1ba598791df
sbmTRT <thureintun@signalbase.co.jp>
2023-12-01 16:19:34 +0900

    TESTING Fastapi 3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <Header />
-    <router-view />
+    <div v-if="currentView === 'home'">
+      <Home />
+    </div>
+    <div v-else-if="currentView === 'about'">
+      <About />
+    </div>
     <Footer />
   </div>
 </template>
@@ -9,11 +14,20 @@
 <script>
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
 
 export default {
   components: {
     Header,
     Footer,
+    Home,
+    About,
+  },
+  data() {
+    return {
+      currentView: 'home', // Set the default view
+    };
   },
   // App-level logic goes here
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,4 @@
 import {createApp} from 'vue';
 import App from './App.vue';
-import router from './router';
-import Vue from 'vue';
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router, // Inject the router into the Vue instance
-  render: h => h(App),
-}).$mount('#app');
-// createApp(App).mount('#app');
+createApp(App).mount('#app');
