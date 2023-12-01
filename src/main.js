1a42f9d21d95dc2b7e58670a985957943d0f2cf9
sbmTRT <thureintun@signalbase.co.jp>
2023-12-01 16:17:03 +0900

    TESTING Fastapi 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import {createApp} from 'vue';
 import App from './App.vue';
+import router from './router';
+import Vue from 'vue';
 
-createApp(App).mount('#app');
+Vue.config.productionTip = false;
+
+new Vue({
+  router, // Inject the router into the Vue instance
+  render: h => h(App),
+}).$mount('#app');
+// createApp(App).mount('#app');
